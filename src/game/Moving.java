87a8a059e1e4e13888673a0d9d682d4f7ffed599
sevenmoor <sevenmoor@archwolf.localdomain>
2019-02-01 15:55:36 +0100

    Adding Moving data class and classes for the save package

--- src/game/Moving.java
+++ src/game/Moving.java
@@ -1,5 +1,40 @@
 package game;
 
-public class Moving {
+import java.util.Date;
 
+public class Moving {
+	private int weekendMovingRate;
+	private Date workStartHour;
+	private Date workEndHour;
+	
+	public Moving(int weekendMovingRate, Date workStartHour, Date workEndHour) {
+		super();
+		this.weekendMovingRate = weekendMovingRate;
+		this.workStartHour = workStartHour;
+		this.workEndHour = workEndHour;
+	}
+
+	public int getWeekendMovingRate() {
+		return weekendMovingRate;
+	}
+
+	public void setWeekendMovingRate(int weekendMovingRate) {
+		this.weekendMovingRate = weekendMovingRate;
+	}
+
+	public Date getWorkStartHour() {
+		return workStartHour;
+	}
+
+	public void setWorkStartHour(Date workStartHour) {
+		this.workStartHour = workStartHour;
+	}
+
+	public Date getWorkEndHour() {
+		return workEndHour;
+	}
+
+	public void setWorkEndHour(Date workEndHour) {
+		this.workEndHour = workEndHour;
+	}
 }
