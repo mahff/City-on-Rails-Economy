2c20f0d9d513e072374f5d540f526ca6506dddfc
Anne-Sophie <Anne-Sophie@1.75.0.10.rev.sfr.net>
2019-02-15 15:55:37 +0100

    modif statistiques generales de la ville (affichage)

--- src/gui/GeneralInformation.java
+++ src/gui/GeneralInformation.java
@@ -4,15 +4,39 @@ import java.awt.Component;
 
 import javax.swing.JLabel;
 
+import game.Town;
+
 public class GeneralInformation {
+	private JLabel summary = new JLabel();
+	private Town town;
 	
 	
-	static JLabel summary = new JLabel();
+	public GeneralInformation(Town town) {
+		setTown(town);
+	}
 	
 	
-	public static Component setGeneralInfo() {
-		summary.setText("General information");
+	public Component updateGeneralInfo() {
+		String content = "City Statistics:\n";
+		
+		int generalPopulation = town.getGeneralPopulation();
+		int generalSatisfaction = town.getGeneralSatisfaction();
+		
+		content += "Population: "+generalPopulation+"\n"
+				+ "Satisfaction: "+generalSatisfaction+"\n";
+
+		summary.setText(content);
+		
 		return summary; 
+	}
+	
+	
+	private void setTown(Town town) {
+		this.town = town;
+	}
+	
+	private Town getTown() {
+		return this.town;
 	}
 
 }
