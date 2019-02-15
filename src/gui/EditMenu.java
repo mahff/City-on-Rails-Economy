9ccabb71ff2d6a32003159fd3cd49c9dbf19ceae
Anne-Sophie <Anne-Sophie@1.75.0.10.rev.sfr.net>
2019-02-15 15:32:43 +0100

    modifs menu "File"

--- src/gui/EditMenu.java
+++ src/gui/EditMenu.java
@@ -1,42 +1,56 @@
 package gui;
 
-import java.awt.HeadlessException;
-import java.awt.MenuBar;
-import java.awt.event.KeyEvent;
-
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class EditMenu extends JFrame{
+	private static final long serialVersionUID = 1L;
+	
+	
 	JMenuBar menuBar = new JMenuBar();
-	JMenuItem menuItem;
+	JMenuItem menuItemSave, menuItemLoad, menuItemQuit;
 	public JMenu menu = new JMenu(); 
 	
 	public EditMenu() throws HeadlessException {
 		super();
-		
 	}
 	
 	public JMenuBar getMenu() {
 		menu = new JMenu("File");
-		menuItem = new JMenuItem("Save game",
+		
+		menuItemSave = new JMenuItem("Save game",
                 KeyEvent.VK_T);
-		menu.add(menuItem);
-		menuItem = new JMenuItem("Load game",
+		menu.add(menuItemSave);
+		
+		menuItemLoad = new JMenuItem("Load game",
                 KeyEvent.VK_T);
-		menu.add(menuItem);
-		menuItem = new JMenuItem("Back",
+		menu.add(menuItemLoad);
+		
+		menuItemQuit = new JMenuItem("Close",
                 KeyEvent.VK_T);
-		menu.add(menuItem);
-		menuItem = new JMenuItem("Close",
-                KeyEvent.VK_T);
-		menu.add(menuItem);
+		
+		menu.add(menuItemQuit);
 		menuBar.add(menu);
+		
+		// listener
+		menuItemQuit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				//TODO : Save avant de quitter ?
+				closeFrame();
+			}
+    	});
+		
+		
 		return menuBar;
 	}
 	
 
-	
+	public void closeFrame() {
+    	int confirmed = JOptionPane.showConfirmDialog(null,"Are you sure you want to exit the program?", "Exit Program?",JOptionPane.YES_NO_OPTION);
+	    if (confirmed == JOptionPane.YES_OPTION) {
+	    		System.exit(0);
+	    }
+    }
 }
