c8b78ef096719b9644d614f136b0edaf61ea89bf
GillesAnneSophie <gilles.annesophiee@gmail.com>
2019-02-26 21:00:29 +0100

    un peu de rangement + ajout image

--- src/gui/FirstView.java
+++ src/gui/FirstView.java
@@ -10,51 +10,74 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class FirstView {
-	// JPanel
-    JPanel view = new JPanel();
+    JPanel view;
+    JFrame frame;
     
-    JFrame frame = new JFrame(); 
-    
-    //Image
     BufferedImage icon;
+    ImageIcon logo; 
+    
 	JLabel picLabel;
 	JLabel welcome;
 	JLabel haveFun;
-	ImageIcon logo; 
-    
-    // Buttons
-    JButton newGame = new JButton("New game");
-    JButton backup = new JButton("Back up"); 
-    JButton close = new JButton("Quit");
+	
+    JButton newGame;
+    JButton backup;
+    JButton close;
 
+    
     public FirstView() {
+    	//Initialize
+    	view = new JPanel();
+    	frame = new JFrame();
+    	
+    	view.setLayout(null);
+    	
+    	newGame = new JButton("New game");
+    	backup = new JButton("Back up"); 
+    	close = new JButton("Quit");
+    	
+    	welcome = new JLabel("Welcome on RailCity, you can start a 'new game', or load a 'back up'!", SwingConstants.CENTER);
+    	haveFun = new JLabel("Have fun!", SwingConstants.CENTER);
+    	
+    	
+    	//Image
+    	try {
+            frame.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("city.png")))));
+        } 
+    	catch (IOException e) {
+            e.printStackTrace();
+        }
     	
 		try {
 			icon = ImageIO.read(new File("rail.png"));
-		} catch (IOException e1) {
+		} 
+		catch (IOException e1) {
 			e1.printStackTrace();
 		}
 		
 		logo = new ImageIcon(icon);
-		
 		picLabel = new JLabel(logo);
+		
+        
+        //Sizes
+		view.setBounds(32, 29, 523, 447);
+		
 		picLabel.setBounds(144,13,225,225);
-		
-		welcome = new JLabel("Welcome on RailCity, you can start a 'new game', or load a 'back up'!", SwingConstants.CENTER);
 		welcome.setBounds(0, 262, 523, 16);
-		
-		haveFun = new JLabel("Have fun!", SwingConstants.CENTER);
 		haveFun.setBounds(199, 280, 122, 16);
 		
-    	newGame.setBounds(199, 317, 122, 25);
+        newGame.setBounds(199, 317, 122, 25);
     	backup.setBounds(199, 355, 122, 25);
     	close.setBounds(199, 393, 122, 25);
-        view.setBackground(Color.WHITE);
         
+    	
         //Style
         Color blue = new Color(0, 115, 230);
         Color cyan = new Color(0, 179, 179);
         Font font = new Font("Tahoma", Font.BOLD, 13);
+        
+        view.setBackground(Color.WHITE);
+        
         newGame.setForeground(blue);
         backup.setForeground(blue);
         close.setForeground(blue);
@@ -64,9 +87,6 @@ public class FirstView {
         welcome.setFont(font);
         haveFun.setFont(font);
     	
-        // JPanel bounds
-        view.setBounds(32, 29, 523, 447);
-        view.setLayout(null);
         
         // Adding to JFrame
         view.add(picLabel); 
@@ -75,6 +95,7 @@ public class FirstView {
         view.add(newGame);
         view.add(backup); 
         view.add(close); 
+        
         
         // JFrame properties
         frame.getContentPane().setLayout(null);
@@ -85,8 +106,9 @@ public class FirstView {
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         frame.setVisible(true);
-        frame.setResizable(false); //The window is not resizable anymore ;)
+        frame.setResizable(false);
 	    
+        
         // listeners
         close.addActionListener(new ActionListener() {
 			@Override
@@ -121,12 +143,14 @@ public class FirstView {
     	});
     }
 
+    
     public void closeFrame() {
     	int confirmed = JOptionPane.showConfirmDialog(null,"Are you sure you want to exit the program?", "Exit Program?",JOptionPane.YES_NO_OPTION);
 	    if (confirmed == JOptionPane.YES_OPTION) {
 	    		System.exit(0);
 	    }
     }
+    
     
     public void Backup(File save) {
     	
