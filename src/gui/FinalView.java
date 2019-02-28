7d2d7915bb8e16b84e12f0b76745edf9faf5217c
GillesAnneSophie <gilles.annesophiee@gmail.com>
2019-02-28 21:24:20 +0100

    Fenêtre de fin créée

--- src/gui/FinalView.java
+++ src/gui/FinalView.java
@@ -2,20 +2,28 @@ package gui;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.*;
 
-public class FinalView { //TODO: bouton OK redirection vers Menu
+public class FinalView {
 	JFrame frame;
 	JLabel lblSuccess;
 	JLabel lblInfo;
 	JButton btnOk;
 	
+	
 	public FinalView(boolean success) {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.WHITE);
+		
 		lblSuccess = new JLabel();
 		lblInfo = new JLabel();
 		btnOk = new JButton("OK");
+		
 		
 		if(success) {
 			lblSuccess.setText("Congratulations!");
@@ -27,13 +35,26 @@ public class FinalView { //TODO: bouton OK redirection vers Menu
 		}
 		
 		
-		lblSuccess.setBounds(216, 42, 137, 16);
-		lblInfo.setBounds(165, 71, 188, 16);
-		btnOk.setBounds(165, 169, 97, 25);
+		lblSuccess.setBounds(120, 22, 188, 16);
+		lblInfo.setBounds(120, 63, 188, 16);
+		btnOk.setBounds(181, 115, 67, 25);
 		
 		//Style
         Color blue = new Color(0, 115, 230);
         Color cyan = new Color(0, 179, 179);
+        Font font = new Font("Tahoma", Font.BOLD, 12);
+        
+        btnOk.setForeground(Color.WHITE);
+        lblSuccess.setForeground(cyan);
+        lblInfo.setForeground(cyan);
+        
+        btnOk.setBackground(blue);
+        
+        lblSuccess.setFont(font);
+        lblInfo.setFont(font);
+        
+        lblSuccess.setHorizontalAlignment(SwingConstants.CENTER);
+        lblInfo.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		
 		frame.getContentPane().add(lblSuccess);		
@@ -42,16 +63,35 @@ public class FinalView { //TODO: bouton OK redirection vers Menu
 		
 		
 		frame.getContentPane().setLayout(null);
-        frame.setSize(594, 242);
+        frame.setSize(442, 180);
         frame.setTitle("RailCity - End");
         frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //TODO : do nothing + confirm dialog
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE); //TODO : do nothing + confirm dialog
         frame.setVisible(true);
         frame.setResizable(false);
+        
+        
+        //Listener
+        btnOk.addActionListener(new ActionListener() {
+    	    public void actionPerformed(ActionEvent e) {
+    	        frame.dispose();
+    	        new FirstView(); 
+    	    }
+    	});
+        
+        frame.addWindowListener(new WindowAdapter() {
+			  public void windowClosing(WindowEvent e) {
+				   closeFrame();
+			  }
+		});
 	}
 	
 	
-	public static void main(String[] args) {
-    	FinalView test = new FinalView(true);
+	public void closeFrame() {
+    	int confirmed = JOptionPane.showConfirmDialog(null,"Are you sure you want to exit the program?", "Exit Program?",JOptionPane.YES_NO_OPTION);
+	    if (confirmed == JOptionPane.YES_OPTION) {
+	    		frame.dispose();
+	    		new FirstView();
+	    }
     }
 }
