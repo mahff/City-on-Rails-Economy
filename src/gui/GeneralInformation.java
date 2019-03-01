4c22eff818da0fd5f31030f04000432e7a18c077
Mahff <33895997+mahff@users.noreply.github.com>
2019-03-01 15:33:18 +0100

    Update GeneralInformation.java

--- src/gui/GeneralInformation.java
+++ src/gui/GeneralInformation.java
@@ -1,19 +1,43 @@
 package gui;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 import core.TimerEngine;
 import game.Town;
 
 public class GeneralInformation {
 	private JLabel summary;
+	private JLabel clock; 
+	private JPanel generalInfo; 
 	private Town town;
+	SimpleDateFormat sdf;
+	private int currentSecond;
+    private Calendar calendar;
+    Timer timer ;
 	
 	
 	public GeneralInformation(Town town) {
 		summary = new JLabel();
+		clock = new JLabel(); 
+		summary.setBorder(new EmptyBorder(0,0,250,0));
+		generalInfo = new JPanel(); 
+		generalInfo.add(summary);
+		generalInfo.add(clock);
+		timer = new Timer();
+		sdf  = new SimpleDateFormat("hh:mm");
 		setTown(town);
+		start();
 	}
 
 	
@@ -27,6 +51,7 @@ public class GeneralInformation {
 		int generalNbLines = town.getGeneralNumberOfLines();
 		int generalNbStations = town.getGeneralNumberOfStation();
 		int funds = town.getFunds();
+		
 		int testDisplay = TimerEngine.getInstance().getDaysTestValue();	//Test affichage de l'heure dans l'IHM
 		// Date date = .TimerEngine();
 		
@@ -34,9 +59,7 @@ public class GeneralInformation {
 				+ "Satisfaction: "+generalSatisfaction+"<br/>"
 				+ "Number of lines (metro): "+generalNbLines+"<br/>"
 				+ "Number of stations (metro): "+generalNbStations+"<br/>"
-				+ "Money: "+funds+" MyLiu<br/>"
-				+ "Date: "+testDisplay+"</html>";
-
+				+ "Money: "+funds+" MyLiu </html>";
 		summary.setText(content);
 		
 		//Style
@@ -45,9 +68,26 @@ public class GeneralInformation {
 		summary.setForeground(cyan);
 		summary.setFont(font);
 		
-		return summary; 
+		return generalInfo; 
 	}
 	
+	private void reset(){
+        calendar = Calendar.getInstance();
+        currentSecond = calendar.get(Calendar.SECOND);
+    }
+    public void start(){
+        reset();
+        //int testDisplay = TimerEngine.getInstance().getDaysTestValue();	//Test affichage de l'heure dans l'IHM
+        timer.scheduleAtFixedRate( new TimerTask(){
+            public void run(){
+                if( currentSecond == 60 ) {
+                    reset();
+                }
+                clock.setText( String.format("%s:%02d", sdf.format(calendar.getTime()), currentSecond ));
+                currentSecond++;
+            }
+        }, 0, 1000 );
+    }
 	
 	private void setTown(Town town) {
 		this.town = town;
