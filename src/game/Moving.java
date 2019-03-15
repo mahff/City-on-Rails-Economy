b3f18bfd5ed979f0cd6ba04dd5682f0c35fb9d05
GillesAnneSophie <gilles.annesophiee@gmail.com>
2019-03-15 17:00:46 +0100

    correction déplacements au travail pour chaque personne

--- src/game/Moving.java
+++ src/game/Moving.java
@@ -49,22 +49,26 @@ public class Moving {
 				District currentDistrict = town.getDistrict(i, j);
 				
 				if(currentDistrict.getColor() == Resident.residentColor) {
-					int cycleForStation = goToNearestStation(town, i, j);
-					int cycleForBusiness = workToNearestBusinessDistrict(town, i, j);
+					int population = currentDistrict.getPopulation();
 					
-					if(cycleForStation != -1) {
-						if(cycleForBusiness != -1) {
-							
-							//Time in minutes
-							int timeToGoToStation = (int) (cycleForStation*Math.random()%5+1);
-							int timeToGoBusiness = (int) (cycleForBusiness*Math.random()%10+1);
-							
-							int totalTime = timeToGoToStation + timeToGoBusiness;
-							
-							if(totalTime > 60) {
-								Station currentStation = currentDistrict.getStation();
-								currentStation.removePassenger(1);
-								((Business) currentDistrict).removeWorker(1);
+					for(int p=0 ; p<population ; p++) {
+						int cycleForStation = goToNearestStation(town, i, j);
+						int cycleForBusiness = workToNearestBusinessDistrict(town, i, j);
+						
+						if(cycleForStation != -1) {
+							if(cycleForBusiness != -1) {
+								
+								//Time in minutes
+								int timeToGoToStation = (int) (cycleForStation*Math.random()%5+1);
+								int timeToGoBusiness = (int) (cycleForBusiness*Math.random()%10+1);
+								
+								int totalTime = timeToGoToStation + timeToGoBusiness;
+								
+								if(totalTime > 60) {
+									Station currentStation = currentDistrict.getStation();
+									currentStation.removePassenger(1);
+									((Business) currentDistrict).removeWorker(1);
+								}
 							}
 						}
 					}
@@ -75,6 +79,7 @@ public class Moving {
 	}
 	
 	
+	//TODO utiliser cette fonction pour faire les déplacements vers state en passant le type en param + gotToState()
 	public int workToNearestBusinessDistrict(Town town, int positionX, int positionY) {
 		int currentCircle = 1;
 		int length = town.getLength();
